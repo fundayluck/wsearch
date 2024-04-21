0c1cbe6df9c0cd5571a3450e445184b6142b67a0
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-21 13:49:20 +0700

    more parent to child communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,23 @@ import { Component } from '@angular/core';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { PageListComponent } from './page-list/page-list.component';
 import { WikipediaService } from './wikipedia.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SearchBarComponent, PageListComponent],
+  imports: [SearchBarComponent, PageListComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'wsearch';
+  pages = [];
 
   constructor(private wikipedia: WikipediaService) {}
 
   onTerm(term: string) {
-    this.wikipedia.search(term).subscribe((res) => {
-      console.log(res);
+    this.wikipedia.search(term).subscribe((res: any) => {
+      this.pages = res.query.search;
     });
   }
 }
