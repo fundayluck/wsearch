e35fc3805b3204d5faf3b520a087f79e8f646d2b
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-21 10:44:36 +0700

    handling form submission

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css'
+  styleUrl: './search-bar.component.css',
 })
 export class SearchBarComponent {
-
+  term = '';
+  onInput(e: Event) {
+    this.term = (e.target as HTMLInputElement).value;
+  }
 }
